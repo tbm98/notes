127b8523a0486b091f79bbe3a779759a03ba35c1
minh.tran <minh.tran@onemount.com>
2021-09-12 19:22:41 +0700

    more test about avatar_widget

--- test/widget_test/widgets/avatar_widget_test.dart
+++ test/widget_test/widgets/avatar_widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:notes/src/ui/dialogs/profile.dart';
 import 'package:notes/src/ui/screens/home/providers/profile_providers.dart';
 import 'package:notes/src/ui/screens/memories/providers.dart';
@@ -11,6 +12,29 @@ import '../config_vars.dart';
 import '../utils.dart';
 
 void main() {
+  setupGetItTest();
+  testWidgets('test avatar_widget guest', (tester) async {
+    //setup
+    when(mockAuth.signIn()).thenAnswer((_) => Future.value(userSignedIn));
+
+    await tester.pumpWidget(ProviderScope(overrides: [
+      profileProvider
+          .overrideWithValue(ProfileNotifier(user: userGuest, auth: mockAuth)),
+      unfinishedTodoProvider.overrideWithValue([]),
+      memoriesNoteProvider.overrideWithValue([])
+    ], child: testApp(child: const AvatarWidget())));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(GuestAvatar), findsOneWidget);
+    expect(find.byType(SignedInAvatar), findsNothing);
+
+    await tester.tap(find.byType(InkWell));
+    await tester.pumpAndSettle();
+
+    verify(mockAuth.signIn());
+  });
+
   testWidgets('test avatar_widget signedin', (tester) async {
     await tester.pumpWidget(ProviderScope(overrides: [
       profileProvider.overrideWithValue(
